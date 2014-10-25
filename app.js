8f87da852c6fd5e75717d03e068f00e4346de523
kdmon <kdmon@users.noreply.github.com>
2014-10-25 21:32:43 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+/* jQuery extension to footable for sticky headers*/
+      
+(function($) {
+  $.fn.stickyHeader = function() {
+    return this.each(function() {
+      var originalTable = $(this);
+      var originalHeader = $(this).find("thead tr");
+      var fixedHeader;
+      var timer;
+      
+      function makeHeader() {
+        clearTimeout(timer);
+        timer = setTimeout(function() {
+          
+          var offset = $(window).scrollTop(),
+          tableOffsetTop = originalTable.offset().top;
+          tableOffsetBottom = tableOffsetTop + originalTable.height() - originalHeader.height();
+          
+          // outside range, remove the fixed header
+          if (offset < tableOffsetTop || offset > tableOffsetBottom) {
+            
+            if ($(".fixed").length > 0) {
+              console.log("hiding header");
+              $(".fixed").remove();
+            }
+          }
+          
+          // otherwise recreate header
+          else {
+            console.log("recreating header")
+            if (fixedHeader) fixedHeader.remove();
+            fixedHeader = originalHeader.clone(1);
+            fixedHeader.addClass("fixed").insertBefore(originalHeader);
+            fixedHeader.find("th").each(function(index) {
+              $(this).css("width", originalHeader.find("th").eq(index).width() + "px");
+            });
+          }
+            
+        }, 200);
+      }
+      $(window).resize(makeHeader);
+      $(window).scroll(makeHeader);
+      makeHeader();
+    });
+  };
+})(jQuery);
+
+
+/* Main app */
+
+$(function() {
+
+  /* IE polyfill */
+  
+  if (typeof console === "undefined"){
+    console={};
+    console.log = function(){
+        return;
+    }
+  }
+
+  /* jQuery Mobile panel setup */
+  
+  $("body>[data-role='panel']").panel().trigger('create');
+  
+  /* Event handlers */
+  
+  $(document).on("pageinit", "#indications", function() {
+    setTimeout(function() {
+      $('.footable').stickyHeader().footable({
+        toggleHTMLElement: '<span style="margin: 1em; float:left;"><i class="fa fa-plus-square-o fa-3x footable-toggle footable-expand"></i> <i class="fa fa-minus-square-o fa-3x footable-toggle footable-contract"></i></span>',
+        toggleSelector: " > tbody > tr > td > span.footable-toggle"
+      });
+    }, 300);
+  });
+});
+
